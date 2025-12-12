cac00aefd8df1cf64c6a26e7b6def91ed34559b0
RITIK MAKHIJA <ritikmakhija2002@gmail.com>
2025-12-12 19:38:21 +0530

    upda

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ const analyticsRoutes = require('./routes/analytics');
 const gamificationRoutes = require('./routes/gamification');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Swagger configuration
 const swaggerOptions = {
